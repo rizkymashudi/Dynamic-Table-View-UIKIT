9bf6b9aada498cba72c3b21a246f5d84825aa3d8
rimas12 <rizky_mashudy@yahoo.co.id>
2021-08-02 08:36:59 +0700

    Initial Commit

---
+++ JDMListDynamic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JDMListDynamic
+//
+//  Created by Rizky Mashudi on 02/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
